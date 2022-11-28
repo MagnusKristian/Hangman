8a4f087f4a962b3b494b906337c777dfa408770f
MagnusKristian <92784711+MagnusKristian@users.noreply.github.com>
2022-11-28 08:15:04 +0100

    old push

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿namespace Hangman
-{
+{ 
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Words.ordListe(); 
             new MakeGame();
         }
     }
---
+++ Words.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+namespace Hangman;
+
+public class Words
+{
+    private static readonly Random Random = new Random();
+
+    public static void ordListe()
+    {
+        // Velge tilfeldig ord.  - eks: abonnement
+        var words = GetWords();
+        var wordCount = 200;
+        while (wordCount > 0)
+        {
+            var hasFoundMatch = FindWordProblem(words);
+            if (hasFoundMatch) wordCount--;
+        }
+    }
+
+    private static bool FindWordProblem(string[] words)
+    {
+        var randomWordIndex = Random.Next(words.Length);
+        var selectedWord = words[randomWordIndex];
+        Console.Write(selectedWord + " - ");
+
+        // Leter videre etter ord som begynner på siste del av dette ordet. - mental
+        foreach (var word in words)
+        {
+            if (!IsLastPartOfFirstWordEqualToFirstPartOfSecondWord(selectedWord, word)) continue;
+            Console.WriteLine(word);
+            return true;
+        }
+        Console.WriteLine("<fant ikke match>");
+        return false;
+    }
+
+    private static bool IsLastPartOfFirstWordEqualToFirstPartOfSecondWord(string word1, string word2)
+    {
+        return IsLastPartOfFirstWordEqualToFirstPartOfSecondWord(4, word1, word2)
+        || IsLastPartOfFirstWordEqualToFirstPartOfSecondWord(5, word1, word2);
+    }
+
+    private static bool IsLastPartOfFirstWordEqualToFirstPartOfSecondWord(int commonLength, string word1, string word2)
+    {
+        // 0123456789
+        // abonnement - commonLength == 4
+        // "abonnement".Length == 10
+        var lastPartOfFirstWord = word1.Substring(word1.Length - commonLength, commonLength);
+        // 012345
+        // mental
+        var firstPartOfSecondWord = word2.Substring(0, commonLength);
+
+        return lastPartOfFirstWord == firstPartOfSecondWord;
+    }
+
+    static string[] GetWords()
+    {
+        var lastWord = string.Empty;
+        var filePath = @"C:\Users\Magnus GetAcademy\Documents\GitHub\Hangman\ordliste.txt";
+        var wordList = new List<string>();
+        foreach (var line in File.ReadLines(filePath, Encoding.UTF8))
+        {
+            var parts = line.Split('\t');
+            var word = parts[1];
+            if (word != lastWord
+                && word.Length > 6
+                && word.Length < 10
+                && !word.Contains("-"))
+            {
+                wordList.Add(word);
+            }
+            lastWord = word;
+        }
+        return wordList.ToArray();
+    }
+}
